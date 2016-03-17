d8b66461c552956a40266e8a2fd59b8a7e331026
Thomas Hall <tureyhall@gmail.com>
2016-03-17 22:45:26 +0000

    Part 2 done!

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 var fs = require('fs');
 var http = require('http');
+var file = "fundstarter.html";
 var requestListener = function (req, res) {
-  fs.readFile("fundstarter.html", 'utf8', function (err,data) {
+  fs.stat(file, function (err,stats) {
     if (err) {
+      res.writeHead(404);
+      res.end("File not found!");
       return console.log(err);
     }
-    res.writeHead(200);
-    res.end(data);
+    //Open file.
+    fs.open(file, 'r', function (err,fd) {
+      if (err) {
+        res.writeHead(404);
+        res.end("File not found!");
+        return console.log(err);
+      }
+
+      var buffer = new Buffer(stats.size);
+      fs.read(fd, buffer, 0, stats.size, 0, function (err,bytesRead,buffer) {
+        if (err) {
+          res.writeHead(500);
+          res.end("Error reading file!");
+          return console.log(err);
+        }
+
+        res.writeHead(200);
+        res.end(buffer.toString('utf8'));
+      });
+    });
   });
 }
 
