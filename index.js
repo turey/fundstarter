467d87915e4aab553861ba13ee2ccbb5771cb25f
Ubuntu <ubuntu@ip-172-31-20-2.ap-northeast-1.compute.internal>
2016-03-17 21:02:26 +0000

    readFileSync working. (Part 1a)

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-var express = require('express')
-var app = express()
+var fs = require('fs');
+var http = require('http');
+var requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end(fs.readFileSync("fundstarter.html"));
+}
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
-
-app.get('/', function(request, response) {
-response.sendfile('fundstarter.html') 
-})
-
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+var server = http.createServer(requestListener);
+server.listen(8080);
