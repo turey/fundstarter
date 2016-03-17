3e37a9aecf1f54287c3c2a64313cf9fc484fcbad
Thomas Hall <tureyhall@gmail.com>
2016-03-17 23:00:09 +0000

    Added more comments.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var http = require('http');
 var file = "fundstarter.html";
 var requestListener = function (req, res) {
+  //Check if file exists and get its size.
   fs.stat(file, function (err,stats) {
     if (err) {
       res.writeHead(404);
@@ -17,6 +18,7 @@ var requestListener = function (req, res) {
       }
 
       var buffer = new Buffer(stats.size);
+      //Read file.
       fs.read(fd, buffer, 0, stats.size, 0, function (err,bytesRead,buffer) {
         if (err) {
           res.writeHead(500);
@@ -24,6 +26,7 @@ var requestListener = function (req, res) {
           return console.log(err);
         }
 
+        //Write file to response.
         res.writeHead(200);
         res.end(buffer.toString('utf8'));
       });
